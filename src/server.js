6f4416370c53601ac75101e6ac3c245dd2231bd1
Rafli Bangsawan <rafli.bangsawan@ui.ac.id>
2022-08-01 17:38:51 +0700

    created server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
-console.log("initialized project");
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+    const server = Hapi.server({
+      port: 5000,
+      host: 'localhost',
+      routes: {
+        cors: {
+          origin: ['*'],
+        },
+      },
+    });
+  
+    server.route([]);
+  
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+  };
+  
+  
+  init();
