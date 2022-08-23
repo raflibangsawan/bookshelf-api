1fa841b6086131eddb75a4ac1fbb5577b2b96a6b
Rafli Bangsawan <rafli.bangsawan@ui.ac.id>
2022-08-23 15:04:42 +0700

    implemented delete book

--- src/handler.js
+++ src/handler.js
@@ -167,6 +167,26 @@ const updateBook = (request, h) => {
 
 const deleteBook = (request, h) => {
     const { bookId } = request.params;
+
+    const idx = books.findIndex((eachBook) => eachBook.id == bookId);
+
+    if (idx !== 1) {
+        books.splice(idx, 1);
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil dihapus',
+          });
+          response.code(200);
+          return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku gagal dihapus. Id tidak ditemukan',
+      });
+      response.code(404);
+      return response;
+    
 };
 
 module.exports = {
